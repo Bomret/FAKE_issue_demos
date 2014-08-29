4798b2c763d3b2ab13bca73d4026ed3f77066b6c
Stefan Reichel <stefan.reichel@devcrowd.de>
2014-08-29 12:27:18 +0200

    Added issue 528

---
+++ fake_issue_528/build.fsx
@@ -0,0 +1,21 @@
+﻿#r @"FAKE\tools\FakeLib.dll"
+open Fake
+open Fake.AssemblyInfoFile
+
+RestorePackages()
+
+let sourceDir = @".\source"
+let targetDir = @".\target"
+
+Target "Clean" (fun _ -> CleanDirs [targetDir])
+
+Target "CopyTxtOnly" (fun _ ->
+    !! (sourceDir @@ "**/*.*")
+      -- "*.xml"
+    |> CopyFiles targetDir
+)
+
+"Clean"
+    ==> "CopyTxtOnly"
+
+RunTargetOrDefault "CopyTxtOnly"
